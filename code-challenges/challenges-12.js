12fa857e0007f4120cfc8eb88c9afc227fb68403
GhaidaMomani <ghaidaalmomani3@gmail.com>
2022-01-31 19:36:17 +0200

    Ch12

---
+++ code-challenges/challenges-12.js
@@ -0,0 +1,107 @@
+'use strict';
+// Important Note: 
+// Kindly use filter instead of for in all of your solutions
+
+// Resource:
+// Filter: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+
+// 1) ---------------------
+// 
+// Given an array of values use filter to extract odd values.
+//  
+// EX:
+// [20, 54, 89, 41, 2, 31, 111, 15, 0, 31, 200] ==> [89, 41, 31, 111, 31]
+// 
+// ------------------------
+
+const oddFiltration = (arr) => {
+    return arr.filter(num => num%2==1);
+}
+
+// 2) ---------------------
+// 
+// Given data of candidates return the candidates CVs that have more than 4 years of experience 
+// and works as JS developer 
+
+// EX:
+// var cvs = [
+//     {
+//         firstName: "Jason",
+//         LastName: "James",
+//         yearsOfExperience: 20,
+//         tech: "JS"
+//     },
+//     {
+//         firstName: "Shira",
+//         LastName: null,
+//         yearsOfExperience: 5,
+//         tech: ".Net"
+//     },
+//     {
+//         firstName: "Dmitri",
+//         LastName: "Akkerman",
+//         yearsOfExperience: 1,
+//         tech: "Python"
+//     },
+//     {
+//         firstName: "Isabella",
+//         LastName: null,
+//         yearsOfExperience: 4,
+//         tech: "Java"
+//     }
+// ]
+//
+//   ===>
+//
+// [
+//     {
+//         fullName: "Jason James",
+//         tech: "JS"
+//     }
+// ]
+
+//  Note that:
+//  1- Full name is first name + last name
+//  2- If one of the names is null dont add it to the full name
+// ------------------------
+
+const cvsFiltration = (arr) => {
+    let newArr = arr.filter(cv => ((cv.yearsOfExperience > 4)&&(cv.firstName != null)&&(cv.LastName != null)) );
+    return newArr.map(cv => {return {fullName: `${cv.firstName} ${cv.LastName}`, tech: cv.tech}} );
+}
+// 3) ---------------------
+//
+// Given an array of words filter the names that contains one of the vowels (a, e, i, o, u)
+
+// EX:
+// ['car', 'boy', 'spy', 'building', 'why', 'dry' ] ==> ['spy', 'why', 'dry']
+// 
+// ------------------------
+
+const vowelsFiltration = (arr) => {
+    return arr.filter(word => (word.indexOf("a")==-1)&&(word.indexOf("e")==-1)&&(word.indexOf("i")==-1)&&(word.indexOf("o")==-1)&&(word.indexOf("u")==-1) );
+} 
+
+// 4) ---------------------
+//
+// An employer wants to compare between his employees skills, write a function that will extract the differences 
+// between the employees skills.
+
+// EX:
+// first_Employee = [C, C#, Java, Angular, PHP]
+// first_Employee = [C#, .Net, JavaScript, React, Angular]
+// ==>
+// result = [C, Java, PHP, .Net, JavaScript, React]
+// Hint:
+// - the 'include' method can help you, read about it.
+// ------------------------
+
+const skillComparison = (arr1, arr2) => {
+    let newArr1 = arr1.filter(i => !arr2.includes(i) );
+    let newArr2 = arr2.filter(i => !arr1.includes(i) );
+    return newArr1.concat(newArr2);
+}
+
+
+module.exports = { oddFiltration, cvsFiltration, vowelsFiltration, skillComparison };
+
